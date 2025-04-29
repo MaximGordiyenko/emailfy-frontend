c72ab52fa28e2ac900a7776c4daa5f28b4be7d83
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2025-04-29 14:20:24 +0300

    set up frontend to host on fly.io

--- src/api/API.js
+++ src/api/API.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { refreshAccessToken } from './auth/auth.js';
 
 export const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_BACKEND_URL,
   withCredentials: true,
 });
 
